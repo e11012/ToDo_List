86d7335105d7bc1c3d69f2086250c99e0a42ec48
e11012 <e11012diptanshu@dpsrkp.net>
2021-06-17 18:56:34 +0530

    my first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(["you can delete this by clicking on it"]);
+  const [inputState, setInputState] = useState("");
+
+  const Add = (e) => {
+    e.preventDefault();
+
+    setTodos([...todos, inputState]);
+
+    setInputState("");
+  };
+
+  const Delete = (i) => (e) => {
+    setTodos((todos_) => todos_.filter((_, k) => i !== k));
+  };
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      Add();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO list</h1>
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter New ToDo"
+            value={inputState}
+            onChange={(e) => setInputState(e.target.value)}
+            onKeyPress={handleKeypress}
+          />
+          <button onClick={Add}>Add</button>
+        </form>
+      </div>
+
+      <ul>
+        {todos.map((todo, i) => (
+          <li iclass="b" key={i} onClick={Delete(i)} class="b">
+            {todo}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
